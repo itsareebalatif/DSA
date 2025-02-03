aee10def8d86355e17df4ef0c8c7463985313614
itsareebalatif <itsareebalatif@gmail.com>
2025-02-04 03:06:03 +0500

    Remove function

--- double_linked_list.py
+++ double_linked_list.py
@@ -23,6 +23,8 @@ class Doubly:
     def pop(self):
         #case 1 :only one Node
         temp=self.head
+        if temp is None:
+            return
         if temp.next is None:
             self.head=None
             return temp.val
@@ -44,36 +46,71 @@ class Doubly:
     def insert(self,index,val):
         new_node=Node(val)
         temp=self.head
+        # If the list is empty and inserting at index 0
         if self.head is None:
+            self.head=new_node
             return
+        # If inserting at the beginning (index 0)
         if  index==0:
             self.head=new_node
             new_node.next=temp
             temp.prev=new_node
             return
-            
-
         counter=0
         previous=None
         while temp  is not None and counter<index:
             previous=temp
             temp=temp.next
             counter+=1
-        
         # if index not found enter at last    
         if temp is None:
-            
             previous.next=new_node
             new_node.prev=previous
-            return    
+            return  
+        # Inserting at the middle  
         previous.next=new_node   
         new_node.prev=previous
 
         new_node.next=temp
-        temp.prev=new_node
+        if temp is not None:
+           temp.prev=new_node
         return
         
-        
+    def remove(self,value):
+        temp=self.head
+        if temp.val==value:
+            if temp.next is not None:
+                print("Case 1 to remove from 0 index")
+                self.head=temp.next
+                self.head.prev=None
+                return
+            print("case 2 to remove from 0 index ")    
+            self.head=temp.next 
+            return 
+
+        last=None
+        while temp is not None:
+            if temp.val==value:
+                break
+            last=temp
+            temp=temp.next
+
+        if temp is None:
+            return
+        if temp.next is None:
+            last.next=temp.next
+            return
+        else:
+            last.next=temp.next
+            temp.next.prev=last  
+            return  
+
+
+
+
+
+            
+
 
 
 
@@ -108,10 +145,13 @@ def main():
     Doubly_Linked.push(1)
     Doubly_Linked.push(2)
     Doubly_Linked.push(3)
-    Doubly_Linked.insert(10,11)
+    Doubly_Linked.insert(2,11)
     Doubly_Linked.insert(19,12)
+    
   
-    print("This is Doubly:",Doubly_Linked)
+    print("This is Doubly after insert :",Doubly_Linked)
+    Doubly_Linked.remove(1)
+    print("This is Doubly after remove :",Doubly_Linked)
     print("This is pop func:",Doubly_Linked.pop())
     Doubly_Linked.insert_start(0)
     print("This is satrting insertion:",Doubly_Linked)
