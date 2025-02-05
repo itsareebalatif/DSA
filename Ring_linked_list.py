b74c58fef454ac8c88a4ae1a07343a8cfdf03ab7
itsareebalatif <itsareebalatif@gmail.com>
2025-02-05 06:00:41 +0500

    remove function

--- Ring_linked_list.py
+++ Ring_linked_list.py
@@ -26,13 +26,14 @@ class Ring:
         new_node=Node(value)
         last=self._get_last()
         temp=self.head
-        
+        #case 1.0
         if self.head is None:
             if  index>0:
                raise IndexError ("index out of bound")
             self.head=new_node
             new_node.next=self.head
             return
+        #case 1.1
         if index==0:
             new_node.next=self.head
             self.head=new_node
@@ -41,17 +42,40 @@ class Ring:
             else:
                 last.next=new_node
             return 
-         
         previous=None
         counter=0
-        
+        #case 2.0
         while  counter<index:
             previous=temp
             temp=temp.next
-            counter+=1
-           
+            counter+=1 
         previous.next=new_node
-        new_node.next=temp                       
+        new_node.next=temp 
+
+    def remove(self,value):
+        if self.head is None:
+            return 
+        temp=self.head
+        last=self._get_last
+        if temp.val==value:
+            if temp.next is None:
+                self.head=None
+            self.head=temp.next
+            last.next=self.head
+        previous=temp
+        temp=temp.next 
+        while temp!=self.head:
+            if temp.val==value:
+                break
+            previous=temp
+            temp=temp.next 
+        if temp==self.head :
+            return     
+        previous.next=temp.next      
+
+
+
+
 
 
     def __str__(self):
@@ -72,8 +96,10 @@ def main():
     Rings=Ring()
     Rings.insert(0,1)
     Rings.insert(0,2)
-    print(Rings)
     Rings.insert(1,3)
+    Rings.insert(7,5)
+    print(Rings)
+    Rings.remove(9)
     print(Rings)
 
 
